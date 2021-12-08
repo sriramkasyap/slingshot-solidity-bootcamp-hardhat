03d44f6247b08027d95096b15b519df2695499a0
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2021-12-08 10:14:48 +0530

    added ERC20 token contract, deployed

---
+++ contracts/SKMToken.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+
+contract SKMToken is ERC20, Ownable {`
+    uint public constant MAX_SUPPLY = 1000 * 10**18;
+
+    constructor(uint initialSupply) ERC20("Sriram's Token", "SKM") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function rewardAnyone(address _recipient, uint amount) public onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "You have reached the maximum supply of this token");
+        _mint(_recipient, amount);
+    }
+
+    function unMintedTokenCount() public view returns(uint) {
+        return MAX_SUPPLY - totalSupply();
+    }
+
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,5 +19,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.6",
+  solidity: "0.8.7",
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/a610e824d6bc4bef94728de6b76a098f",
+      accounts: [
+        "426ca860238d5414b59d9588cb8e85b2aca94bf20a025c175746fa8c14767725",
+      ],
+    },
+  },
+  etherscan: {
+    apiKey: "U8QD8TJTGES2BTISEJKZZPR3QHC5N54H17",
+  },
 };
